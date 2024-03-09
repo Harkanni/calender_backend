9a16929a7b269b90027445827a32ffbf3b408057
harkanni <harkanzyempire001@gmail.com>
2024-03-09 14:31:39 +0100

    getAllSchedules handler

--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { createSchedule } from '../controllers/controller.js';
+import { createSchedule, getAllSchedules } from '../controllers/controller.js';
 // import { createSchedule } from '../controllers/controller';
 
 const router = express.Router();
 
 router.post('/createSchedule', createSchedule);
+
+router.get('/schedules', getAllSchedules)
+
 router.get('/', (req, res) => {
    res.status(200).json('WELCOME');
 })
